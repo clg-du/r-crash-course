ca859b356f0e3c75d427ccfbdd84acb1d096fa94
Charlie Lindgren <clg@du.se>
2024-08-23 18:24:33 +0900

    `Refactor lecture files for consistency and clarity`

--- lectures/lecture_01/lecture_01.r
+++ lectures/lecture_01/lecture_01.r
@@ -1,9 +1,11 @@
 #' ---
-#' title: "Lecture 1: Introduction to R"
-#' output: rmarkdown::github_document
+#' title: 'Lecture 1: Introduction to R'
+#' output:
+#'   github_document: default
+#'   rmarkdown::github_document: default
 #' date: "2024-08-23"
 #' ---
-
+#'
 #' Here we will learn the basics of the R programming language.
 #'
 #' ## Comments in R
