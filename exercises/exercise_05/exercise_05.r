e5768ee07d3b720cd2a1a86197e98efc0ada2fd7
Charlie Lindgren <clg@du.se>
2024-08-24 20:42:31 +0900

    `Refactor exercise 5 files for consistency and code organization`

---
+++ exercises/exercise_05/exercise_05.r
@@ -0,0 +1,53 @@
+#' # Exercise 5: Vectors in R
+#' `r Sys.Date()`
+#'
+#'
+#' ## Task 1: Creating Vectors
+#'
+#' Create the following vectors and assign them to variables:
+#'
+#' 1. Create a numeric vector with values 10, 20, 30, 40, 50 and assign it to a
+#'    variable called `numeric_vector`.
+#'
+#' 2. Create a character vector with values "red", "green", "blue", "yellow",
+#'    "orange" and assign it to a variable called `character_vector`.
+#'
+#' 3. Create a logical vector with values TRUE, FALSE, TRUE, FALSE, TRUE and
+#'    assign it to a variable called `logical_vector`.
+#'
+#' Your code here:
+
+
+#' ## Task 2: Accessing Vector Elements
+#'
+#' Use indexing to perform the following operations:
+#'
+#' 1. Access the second element of the numeric vector and assign it to a
+#'    variable called `numeric_element`.
+#'
+#' 2. Access the third element of the character vector and assign it to a
+#'    variable called `character_element`.
+#'
+#' 3. Access the last element of the logical vector and assign it to a variable
+#'    called `logical_element`.
+#'
+#' Your code here:
+
+
+#' ## Task 3: Manipulating Vectors
+#'
+#' Perform the following operations on the vectors:
+#'
+#' 1. Add 5 to each element of the numeric vector and assign the result to a new
+#'    variable called `numeric_vector_plus_5`.
+#'
+#' 2. Concatenate the character vector with itself and assign the result to a
+#'    new variable called `character_vector_concatenated`.
+#'
+#' 3. Negate the logical vector and assign the result to a new variable called
+#'    `logical_vector_negated`.
+#'
+#' Your code here:
+
+#'
+#' That's it for Exercise 5! Well done!
