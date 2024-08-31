8123d91560e2623b1da46801f80ade6ecd625fc3
Charlie Lindgren <clg@du.se>
2024-08-31 19:07:02 +0200

    Refactor code to update file paths for figures in knit_all.r

--- knit_all.r
+++ knit_all.r
@@ -58,7 +58,8 @@ for (i in seq_along(r_files)) {
     dirname(r_file),
     "/figures/"
   ), knit_root_dir = paste0(
-    "./",
+    rprojroot::find_rstudio_root_file(),
+    "/",
     dirname(r_file))
   )
   knitr::knit(rmd_file, output = md_file)
